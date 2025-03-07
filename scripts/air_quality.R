923dc638b7cb5a83de8eadcde0eab0d5c30839cb
Arpad Virag <arpad.virag@student.dlh.lu>
2025-03-07 14:20:56 +0000

    Created a chart, but still working on a better one...

--- scripts/air_quality.R
+++ scripts/air_quality.R
@@ -1,3 +1,6 @@
+library(ggplot2)
+library(lubridate)
+
 data(airquality)
 
 head(airquality)
@@ -13,3 +16,10 @@ df[is.na(df)] <- 0
 df['Norm_Solar_Rad'] <- as.data.frame(scale(df$Solar.R))
 
 summary(df)
+
+ggplot(df, aes(x = Solar.R, y = Temp)) +
+  geom_point(color = "blue", size = 2) +
+  theme_minimal() +
+  ggtitle("Temperature vs. Solar radiation)")
+
+df
